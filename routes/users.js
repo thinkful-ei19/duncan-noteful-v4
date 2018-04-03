8aac85313fe871da0dd4ee0e8391de1de43da7db
trentduncan <trent.dunc@gmail.com>
2018-04-03 11:27:45 -0700

    added .env, checking it is being ignored

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ const User = require('../models/user');
 
 
 router.post('/users', (req, res, next) => {
-  const {username, fullName, password} = req.body;
+  // const {username, fullName, password} = req.body;
   
   const requiredFields = ['username', 'password'];
   const missingField = requiredFields.find(field => !(field in req.body));
@@ -36,6 +36,36 @@ router.post('/users', (req, res, next) => {
     return next(err);
   }
 
+  const sizedFields = {
+    username: { min: 1 },
+    password: { min: 8, max: 72 }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+  if (tooSmallField) {
+    const min = sizedFields[tooSmallField].min;
+    const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooLargeField) {
+    const max = sizedFields[tooLargeField].max;
+    const err = new Error(`Field: '${tooLargeField}' must be at most ${max} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  let { username, password, fullName = '' } = req.body;
+  fullName = fullName.trim();
   
 
   return User.hashPassword(password)
